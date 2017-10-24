f9e831321b61053b060989b264dfc5f4310f7b62
Daniel Borstelmann <dborstelmann@wustl.edu>
2017-10-24 14:16:39 -0500

    vuex store init and indentation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <Todos/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Todos from './components/Todos'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        Todos
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,33 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 
+Vue.use(Vuex)
 Vue.config.productionTip = false
+
+const store = new Vuex.Store({
+    state: {
+        todos: [
+            { id: 1, text: 'Number 1', done: true },
+            { id: 2, text: 'Number 2', done: false }
+        ]
+    },
+    getters: {
+        doneTodos: state => {
+            return state.todos.filter(todo => todo.done)
+        },
+        doneTodosCount: (state, getters) => {
+            return getters.doneTodos.length
+        }
+    }
+})
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    store,
+    template: '<App/>',
+    components: { App }
 })
